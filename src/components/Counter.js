e082b1ae08a438c41690a74ebcf212ea90397808
Ayodejioladimeji <layobright11@gmail.com>
2022-02-09 03:01:00 -0800

    implemented the input functionality to pass the input value initialize to 1

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,12 +3,15 @@ import '../styles/Counter.css';
 
 const Counter = () => {
   const [counterValue, setCounterValue] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
 
   return (
     <div className='counter'>
       <div className='counter_box'>
         <h2 data-testid='header'>My Counter</h2>
         <h1 data-testid='counter'>{counterValue}</h1>
+
+        <input type='number' data-testid='input' value={inputValue} />
 
         <div className='counter_btn'>
           <button data-testid='minus-btn'>-</button>
