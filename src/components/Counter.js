b69d97cc19e1f28bcbb180bbd94117d915331e1e
Ayodejioladimeji <layobright11@gmail.com>
2022-02-09 03:41:36 -0800

    Implemented the button funtionality to add and subtract from the counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,15 @@ const Counter = () => {
   const [counterValue, setCounterValue] = useState(0);
   const [inputValue, setInputValue] = useState(1);
 
+  //   The add to counter function
+  const addCounter = () => {
+    setCounterValue(counterValue + inputValue);
+  };
+
+  // The substract from counter function
+  const minusCounter = () => {
+    setCounterValue(counterValue - inputValue);
+  };
   return (
     <div className='counter'>
       <div className='counter_box'>
@@ -19,8 +28,12 @@ const Counter = () => {
         />
 
         <div className='counter_btn'>
-          <button data-testid='minus-btn'>-</button>
-          <button data-testid='add-btn'>+</button>
+          <button data-testid='minus-btn' onClick={minusCounter}>
+            -
+          </button>
+          <button data-testid='add-btn' onClick={addCounter}>
+            +
+          </button>
         </div>
       </div>
     </div>
