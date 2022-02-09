cc015faf900b07975168c65ad6599d450fbf858a
Ayodejioladimeji <layobright11@gmail.com>
2022-02-09 02:39:13 -0800

    Implented the functionality to check if the counter text starts with 0

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Counter.css';
 
 const Counter = () => {
+  const [counterValue, setCounterValue] = useState(0);
+
   return (
     <div className='counter'>
       <div className='counter_box'>
         <h2 data-testid='header'>My Counter</h2>
+        <h1 data-testid='counter'>{counterValue}</h1>
       </div>
     </div>
   );
