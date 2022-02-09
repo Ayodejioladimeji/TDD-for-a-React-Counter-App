949b3db3bad5226b110e1f6c4a2cdb907ef485ad
Ayodejioladimeji <layobright11@gmail.com>
2022-02-09 01:55:02 -0800

    Implemented the test to check if the counter text starts with 0

--- src/components/__test__/Counter.test.js
+++ src/components/__test__/Counter.test.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Counter from '../Counter';
 
+// Checking if the header text renders with == My Counter ==
 test('Check if header renders with correct text', () => {
   const { getByTestId } = render(<Counter />);
   const headerElement = getByTestId('header');
 
   expect(headerElement.textContent).toBe('My Counter');
+});
+
+// checking if the counter text starts with == 0 ==
+test('Counter test initially starts with 0', () => {
+  const { getByTestId } = render(<Counter />);
+  const counterElement = getByTestId('counter');
+
+  expect(counterElement.textContent).toBe('0');
 });
